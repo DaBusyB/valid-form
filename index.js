c99ef8a00549f59a0d7062cc07fb3649513a40bf
DaBusyB <brandiapetsi@gmail.com>
2020-05-31 12:31:17 -0400

    Add error to username input

--- index.js
+++ index.js
@@ -1,22 +1,25 @@
 
 const form = document.getElementById('form');
-const username = document.getElementById('email');
+const username = document.getElementById('username');
 const pw = document.getElementById('password');
 const confirmPW = document.getElementById('confirm-password');
-const username = document.getElementById('username');
+const email = document.getElementById('email');
 
 function showError(input, message) {
-    
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+    const small = formControl.querySelector('small');
+    small.innerText = message;
 }
 
-function showError(input) {
+// function showError(input) {
     
-}
+// }
 
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    if(username.value === "") {
+    if(username.value === '') {
         showError(username, 'Username is required')
     } else {
         showSuccess(username)
