9a2f48d9ee528fc1d218e46d7bb87d28bca7d715
DaBusyB <brandiapetsi@gmail.com>
2020-05-31 13:21:10 -0400

    Refactor: add checkInputs function, remove if statements

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 
 const form = document.getElementById('form');
-const username = document.getElementById('username');
-const pw = document.getElementById('password');
-const confirmPW = document.getElementById('confirm-password');
-const email = document.getElementById('email');
+const username = document.getElementById('Username');
+const pw = document.getElementById('Password');
+const confirmPW = document.getElementById('Confirmed-password');
+const email = document.getElementById('Email');
 
 function showError(input, message) {
     const formControl = input.parentElement;
@@ -22,33 +22,19 @@ function isEmailValid(email) {
     return re.test(String(email).toLowerCase());
 }
 
+function checkInputs(inputsArray) {
+    inputsArray.forEach(function(input) {
+        if(input.value.trim() === '') {
+            showError(input, `${input.id} is required`)
+        } else {
+            showSuccess(input)
+        }
+    })
+}
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
 
-    if(username.value === '') {
-        showError(username, 'Username is required')
-    } else {
-        showSuccess(username)
-    } 
-
-    if(email.value === '') {
-        showError(email, 'Email is required')
-    } else if (!isEmailValid(email.value)) {
-        showError(email, 'An email address is required')
-    } else {
-        showSuccess(email)
-    }
-
-    if(pw.value === '') {
-        showError(pw, 'Password is required')
-    } else {
-        showSuccess(pw)
-    }
-
-    if(confirmPW.value === '') {
-        showError(confirmPW, 'Confirmed password is required')
-    } else {
-        showSuccess(confirmPW)
-    }
+    checkInputs([email, pw, confirmPW, username])
 
 })
