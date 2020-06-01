51a169a965abf3dbc3379b19ec2e20ac87e27dd6
DaBusyB <brandiapetsi@gmail.com>
2020-06-01 01:10:45 -0400

    Add checkConfirmedPWInput, checkInputLength and adjust isEmailValid

--- index.js
+++ index.js
@@ -7,42 +7,66 @@ const email = document.getElementById('Email');
 
 function showError(input, message) {
     const formControl = input.parentElement;
-    formControl.className = 'form-control error';
+    formControl.className = "form-control error";
     const small = formControl.querySelector('small');
     small.innerText = message;
+    //console.log(input.parentElement)
 }
 
 function showSuccess(input) {
     const formControl = input.parentElement;
-    formControl.className = 'form-control success';
-}
-
-function isEmailValid(email) {
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-}
-
-function checkEmailInput(email, cb) {
-    if(!cb(email)) {
-        showError(email, 'Please use a valid email')
-    } else {
-        showSuccess(email)
-    }
+    formControl.className = "form-control success";
 }
 
 function checkInputs(inputsArray) {
     inputsArray.forEach(function(input) {
         if(input.value.trim() === '') {
             showError(input, `${input.id} is required`)
+            //console.log("error 2")
         } else {
             showSuccess(input)
+            //console.log("success 2")
         }
     })
 }
 
+function isEmailValid(email) {
+    // const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    // return re.test(String(email).toLowerCase());
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+        return true
+    }
+    return false
+}
+
+function checkEmailInput(input, cb) {
+    if(!cb(input.value.trim())) {
+        showError(input, 'Please use a valid email')
+        //console.log("error 1")
+    } else {
+        showSuccess(input)
+        // console.log("success 1")
+    }
+}
+
+function checkConfirmedPWInput(pw1, pw2) {
+    pw2.value !== pw1.value || pw1.value.length < 1 ?
+    showError(pw2, `${pw2.id} must match ${pw1.id}`) :
+    showSuccess(pw2);
+
+}
+
+function checkInputLength(input, minLength, maxLength) {
+    input.value.length < minLength || input.value.length > maxLength ?
+    showError(input, `${input.id} should be between ${minLength} and ${maxLength} characters`) :
+    showSuccess(input)
+}
+
 form.addEventListener('submit', function(e) {
     e.preventDefault();
-
-    checkInputs([email, pw, confirmPW, username])
-   checkEmailInput(email.value, isEmailValid)
+    checkInputs([username, email, pw, confirmPW]);
+    checkEmailInput(email, isEmailValid);
+    checkConfirmedPWInput(pw, confirmPW);
+    checkInputLength(pw, 8, 15);
+    checkInputLength(username, 8, 20);
 })
